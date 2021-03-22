04945175dcc6d8501389308b5029f611673be112
Pavel <Pavel@DESKTOP-7B3ONOB>
2021-03-22 22:52:47 +0700

    Constructors. Destructors.

--- Intro/Intro/main.cpp
+++ Intro/Intro/main.cpp
@@ -2,11 +2,51 @@
 using namespace std;
 
 // создание структуры.
-struct Point
+class Point
 {
 	// структура Point описывает точки на плоскости.
 	double x; // координата x.
 	double y; // координата y.
+public:
+	double get_x()
+	{
+		return x;
+	}
+	double get_y()
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this -> x = x;
+	}
+	void set_y(double y)
+	{
+		this -> y = y;
+	}
+	// constructors:
+	Point()
+	{
+		x = y = 0;
+		cout << "Constructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this -> = x;
+		this -> = 0;
+		cout << "Constructor:\t\t" << this << endl;
+
+		Point B = 5;
+		B.print();
+	}
+	~Point()
+	{
+		cout << "Destructor\t" << this << endl;
+	}
+	void print()
+		{
+		cout << "X =" << x << "\tY =" << y << endl;
+		}
 };
 
 //—оздава€ структуру или класс, мы создаем новый тип данных,
@@ -22,4 +62,13 @@ void main()
 	int a; // объ€вление переменной типа int.
 	Point A; //объ€вление объекта ј структуры Point.
              // объ€вление переменной ј типа Point.
+	A.set_x (2);
+	A.set_y (3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point* pA = &A; // создаем указатель на объект.
+	cout << pA-> get_x () << "\t" << pA-> get_y () << endl;
+
+	Point B; // Default constructor.
+	cout << B.get_x() << "\t" << B.get_y() << endl;
 }
