fb322baae5dc0666e136ba9089fbd3f98f2db0d2
Pavel <Pavel@DESKTOP-7B3ONOB>
2021-04-30 21:44:47 +0700

    Academy distributed

---
+++ Inheritance/Academy/Graduate.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include"Student.h"
+class Graduate :public Student
+{
+	string topic;	//Òåìà äèïëîìíîãî ïðîåêòà
+public:
+	const string& get_topic()const;
+	void set_topic(const string& topic);
+
+	Graduate(HUMAN_TAKE_PARAMETERS, STUDENT_GET_PARAMETERS, const string& topic);
+	~Graduate();
+	void info()const;
+};
