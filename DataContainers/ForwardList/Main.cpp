b0eea4a56015b58cf224251ce2ec156badbb34c1
Pavel <Pavel@DESKTOP-7B3ONOB>
2021-05-28 22:52:23 +0700

    Iterator almost DONE

--- DataContainers/ForwardList/Main.cpp
+++ DataContainers/ForwardList/Main.cpp
@@ -20,10 +20,58 @@ public:
 		count--;
 		cout << "EDestructor:\t" << this << endl;
 	}
+	friend class Iterator;
 	friend class ForwardList;
 };
 
 int Element::count = 0;	//Инициализация статической переменной
+
+class Iterator
+{
+	Element* Temp;
+public:
+	Iterator(Element* Temp = nullptr)
+	{
+		this->Temp = Temp;
+		cout << "IConstructor:\t" << this << endl;
+	}
+	~Iterator()
+	{
+		cout << "IDestructor:\t" << this << endl;
+	}
+
+	//			Operators:
+	Iterator& operator++()
+	{
+		Temp = Temp->pNext;
+		return *this;
+	}
+
+	Iterator operator++(int)
+	{
+		Iterator old = *this;
+		Temp = Temp->pNext;
+		return old;
+	}
+
+	bool operator==(const Iterator& other)const
+	{
+		return this->Temp == other.Temp;
+	}
+	bool operator!=(const Iterator& other)const
+	{
+		return this->Temp != other.Temp;
+	}
+
+	Element*& operator->()
+	{
+		return Temp;
+	}
+	Element* get_current_address()
+	{
+		return Temp;
+	}
+};
 
 class ForwardList
 {
@@ -37,17 +85,45 @@ public:
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+	ForwardList(const ForwardList& other)
+	{
+		Element* Temp = other.Head;
+		while (Temp)
+		{
+			push_back(Temp->Data);
+			Temp = Temp->pNext;
+		}
+		cout << "LCopyConstructor:" << this << endl;
+	}
 	~ForwardList()
 	{
+		while (Head)pop_front();
 		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//			Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		Element* Temp = other.Head;
+		while (Temp)
+		{
+			push_back(Temp->Data);
+			Temp = Temp->pNext;
+		}
+		cout << "LCopyAssignment:" << this << endl;
+		return *this;
 	}
 
 	//			Adding elements:
 	void push_front(int Data)
 	{
-		Element* New = new Element(Data);
+		/*Element* New = new Element(Data);
 		New->pNext = Head;
-		Head = New;
+		Head = New;*/
+
+		Head = new Element(Data, Head);
 		size++;
 	}
 	void push_back(int Data)
@@ -58,11 +134,11 @@ public:
 			push_front(Data);
 			return;
 		}
-		Element* New = new Element(Data);
+		//Element* New = new Element(Data);
 		Element* Temp = Head;
 		while (Temp->pNext != nullptr)
 			Temp = Temp->pNext;
-		Temp->pNext = New;
+		Temp->pNext = new Element(Data);
 		size++;
 	}
 
@@ -73,17 +149,18 @@ public:
 			push_front(Data);
 			return;
 		}
-		Element* New = new Element(Data);
+		//Element* New = new Element(Data);
 		Element* Temp = Head;
 		for (int i = 0; i < index - 1; i++, Temp = Temp->pNext)
 			if (Temp->pNext == nullptr)break;
 		//Temp = Temp->pNext;
-		New->pNext = Temp->pNext;
-		Temp->pNext = New;
+		/*New->pNext = Temp->pNext;
+		Temp->pNext = New;*/
+		Temp->pNext = new Element(Data, Temp->pNext);
 		size++;
 	}
 
-	//		Erasing elements:
+	//					Erasing elements:
 	void pop_front()
 	{
 		//1) Прежде чем исключить элемент из списка, нужно запомнить его адрес, для того, чтобы можно было удалить его из памяти.
@@ -103,20 +180,47 @@ public:
 		Temp->pNext = nullptr;
 		size--;
 	}
+	void erase(int index)
+	{
+		if (index > size)return;
+		if (index == 0)
+		{
+			pop_front();
+			return;
+		}
+		//1)Доходим до нужного элемента:
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)
+			Temp = Temp->pNext;
+		//2)Запоминаем адрес удаляемого элемента:
+		Element* to_del = Temp->pNext;
+		//3)Исключаем удаляемый элемент из списка:
+		Temp->pNext = Temp->pNext->pNext;
+		//4)Удаляем элемент из памяти:
+		delete to_del;
+		size--;
+	}
 
 	//			Methods:
 	void print()
 	{
-		Element* Temp = Head;	//Temp - это итератор.
-		while (Temp != nullptr)
-		{
-			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
-			Temp = Temp->pNext;
-		}
+		//Element* Temp = Head;	//Temp - это итератор.
+		//while (Temp)
+		//{
+		//	cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		//	Temp = Temp->pNext;
+		//}
+		//for(Initialisation; Condition; Expression)...
+		//for (Element* Temp = Head; Temp; /*Temp=Temp->pNext*/Temp++)
+			//cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		for (Iterator Temp = Head; Temp != nullptr; Temp++)
+			cout << Temp.get_current_address() << tab << Temp->Data << tab << Temp->pNext << endl;;
 		cout << "Количество элементов в списке: " << size << endl;
 		cout << "Общее количество элементов:    " << Element::count << endl;
 	}
 };
+
+#define BASE_CHECK
 
 void main()
 {
@@ -129,22 +233,35 @@ void main()
 		//list.push_front(rand() % 100);
 		list.push_back(rand() % 100);
 	}
+	list = list;
 	list.print();
+#ifdef BASE_CHECK
 	/*cout << delimiter << endl;
-	list.pop_front();
-	list.pop_back();
-	list.print();*/
+list.pop_front();
+list.pop_back();
+list.print();*/
 	int value;
 	int index;
 	cout << "Введите добавляемое значение: "; cin >> value;
 	cout << "Введите индекс добавляемого значения: "; cin >> index;
 	list.insert(value, index);
 	list.print();
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	list.erase(index);
+	list.print();
 
-	cout << "Еще один список:\n";
+	/*cout << "Еще один список:\n";
 	ForwardList list2;
 	list2.push_back(3);
 	list2.push_back(5);
 	list2.push_back(8);
-	list2.print();
+	list2.print();*/
+#endif // BASE_CHECK
+
+	//ForwardList list2 = list;//CopyConstructor
+	//list2.print();
+
+	//ForwardList list3;
+	//list3 = list2;		//CopyAssignment
+	//list3.print();
 }
