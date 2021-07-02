ca2f8ddba9e0e597ce42a58ab8c02a5821090f1c
Pavel <Pavel@DESKTOP-7B3ONOB>
2021-07-02 23:01:41 +0700

    DICTIONARY --done

---
+++ STL/STL/Main.cpp
@@ -0,0 +1,220 @@
+#include<iostream>
+#include<array>
+#include<vector>
+#include<list>
+
+//using namespace std;
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+
+//#define STL_ARRAY
+//#define STL_VECTOR
+//#define EXEPTIONS_IN_VECTOR
+#define STL_LIST
+
+template<typename T> void vector_properties(const std::vector<T>& vec)
+{
+	cout << "Size:     " << vec.size() << endl;				//	Размер.
+	cout << "Capasity: " << vec.capacity() << endl;		//	Вместительность - сколько элементов может вместить в себя вектор, без переопределения памяти.
+	cout << "MaxSize:  " << vec.max_size() << endl;		//	Максимальный размер
+}
+
+template<typename T> void vector_print(const std::vector<T>& vec)
+{
+	for (typename std::vector<T>::const_iterator it = vec.begin(); it != vec.end(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+}
+
+template <typename T> void vector_reverse_print(const std::vector<T>& vec)
+{
+	//	Написать функцию vector_reverse_print(...),
+	//	которая будет выводить ветктор в обратном порядке.
+	for (typename std::vector<T>::const_reverse_iterator it = vec.crbegin(); it != vec.crend(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+}
+
+template<typename T> void vector_insert(std::vector<T>& vec, int index, T data)
+{
+	//	Добавить значение в вектор по индексу.
+	//	Индекс и значение вводятся с клавиатуры.
+
+	////	C++98
+	//if (index > vec.size())
+	//{
+	//	vec.push_back(data);
+	//}
+	//else
+	//{
+	//	vec.insert(vec.begin() + index, data);
+	//}
+
+	//	Более эффективный метод!
+	//	>= C++11
+	if (index > vec.size())
+	{
+		vec.emplace_back(data);
+	}
+	else if (index < 0)
+	{
+		return;
+	}
+	else
+	{
+		vec.emplace(vec.begin() + index, data);
+	}
+}
+
+template<typename T> void vector_erase(std::vector<T>& vec, int index)
+{
+	//	Удалить значение из вектора по указанному индексу, введенному с клавиатуры.
+	if (index > vec.size() || index < 0)
+	{
+		return;
+	}
+	else
+	{
+		vec.erase(vec.begin() + index);
+	}
+}
+
+template<typename T> void list_print(std::list<T>& list)
+{
+	for (typename std::list<T>::const_iterator it = list.begin(); it != list.end(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#ifdef STL_ARRAY
+	//	array - это контейнер, который хранит данные в виде статического массива.
+	const int n = 5;
+	std::array<int, n> arr = { 3,5,8,13,21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+		//cout << arr.at(i) << tab;
+
+	}
+	cout << endl;
+#endif // STL_ARRAY
+
+#ifdef STL_VECTOR
+	// Vector - это контейнер, который хранит данные в виде динамического массива.
+	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+	vector_properties(vec);
+
+	vec.push_back(55);
+	cout << "vec.push_back(55)" << endl;
+
+	vector_properties(vec);
+
+#ifdef EXEPTIONS_IN_VECTOR
+	// exeption - это ошибка на этапе выполнения.
+	try
+	{
+		for (int i = 0; i < vec.capacity(); i++)
+		{
+			//	[] - индекс оператоh (subscript operator)
+			//	try...catch...
+			//cout << vec[i] << tab;	//	Оператор '[]' - не бросает 'исключение' при выходе за пределы вектора.
+
+			cout << vec.at(i) << tab;	//	Метод 'at()' - бросает 'исключение' (out of range exeption) при выходе за пределы вектора.
+		}
+
+		throw 123;
+		cout << endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+	catch (int e)
+	{
+		std::cerr << "Error: " << e << endl;
+	}
+#endif // EXEPTIONS_IN_VECTOR
+
+	vector_print(vec);
+	cout << "Revers:\n";
+	vector_reverse_print(vec);
+
+	vector_properties(vec);
+	int data; int index;
+	cout << "Индекс вставки: "; cin >> index;
+	cout << "Значение для вставки: "; cin >> data;
+	vector_insert(vec, index, data);
+
+	/*std::vector<int>::iterator position = vec.begin() + index;
+	vec.insert(position, data);*/
+
+	vector_print(vec);
+	vector_properties(vec);
+	cout << "Индекс для удаления: "; cin >> index;
+	//vec.erase(vec.begin() + index);
+	vector_erase(vec, index);
+	vector_print(vec);
+	vector_properties(vec);
+
+	vector_properties(vec);
+	vec.assign({ 1024,2048,3072 });
+	vector_print(vec);
+	vector_properties(vec);
+	vec.shrink_to_fit();
+	vector_properties(vec);
+
+#endif // STL_VECTOR
+
+#ifdef STL_LIST
+	//	List - это контейнер, который хранит объекты в виде двусвязного списка
+	std::list<int> list = { 3,5,8,13,21 };
+
+	/*for (int i = 0; i < list.size(); i++)
+	{
+		//	Оператор [] - не допустим, потому что он сильно снижает производительность!!!
+		cout << list[i] << tab;
+	}
+	cout << endl;*/
+
+	list_print(list);
+
+	/*std::vector<int> vec = {3,5,8,13,21};
+	cout << "Vsize: " << vec.size() << endl;
+	cout << "Vmax_size: " << vec.max_size() << endl;*/
+
+	cout << "Lsize: " << list.size() << endl;
+	cout << "Lmax_size: " << list.max_size() << endl;
+
+	int data; int index;
+	cout << "Индекс вставки: "; cin >> index;
+	cout << "Значение для вставки: "; cin >> data;
+	std::list<int>::iterator it;
+
+	it = list.begin();
+	for (int i = 0; i < index; i++)
+	{
+		it++;
+	}
+
+	list.insert(it, data);
+	list_print(list);
+
+#endif // STL_LIST
+
+}
