c2429561ce7b97485c07a2371ffa48aa2d324f11
Pavel <Pavel@DESKTOP-7B3ONOB>
2021-08-23 22:34:20 +0700

    WinAPItext (DONE)

--- WinAPI/WinAPI/Main.cpp
+++ WinAPI/WinAPI/Main.cpp
@@ -1,12 +1,21 @@
 #include <Windows.h>
 #include "resource.h"
 
+HWND hEdit1;
+HWND hEdit2;
+
+CONST UINT TEXT_SIZE = 256;
+
+//CHAR sz_text_1[TEXT_SIZE] = {"Какой-то текст..."};
+//CHAR sz_text_2[TEXT_SIZE] = { };
+CHAR sz_buffer[TEXT_SIZE] = { };
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, int nCmdShow)
 {
 	//MessageBox(NULL, "Это мое первое окно, созданное в WinAPI", "Info MessageBox", MB_ICONINFORMATION | MB_YESNOCANCEL);
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1),0, (DLGPROC)DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(65535),0, (DLGPROC)DlgProc, 0);
 	return 0;
 }
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -17,13 +26,29 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+		SendMessage(hEdit1, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		//SetFocus(hEdit1);
+
 	}
 		break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK: 
+		{
+			SendMessage(hEdit1, WM_GETTEXT, (WPARAM)TEXT_SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEdit2, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			HWND hStatic2 = GetDlgItem(hwnd, IDC_STATIC2);
+			//SendMessage(hStatic2, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SendMessage(GetDlgItem(hwnd, IDC_STATIC2), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SetDlgItemText(hwnd, IDC_STATIC2, sz_buffer);
+			//MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK);
+		}
+			break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 	}
--- WinAPI/WinAPI/resource.h
+++ WinAPI/WinAPI/resource.h
@@ -2,8 +2,11 @@
 // Включаемый файл, созданный в Microsoft Visual C++.
 // Используется Resource.rc
 //
-#define IDD_DIALOG1                     101
+#define IDD_DIALOG                      101
 #define IDI_ICON1                       103
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_STATIC2                     65535
 
 // Next default values for new objects
 // 
@@ -11,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
