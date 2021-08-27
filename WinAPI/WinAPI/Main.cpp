8ad5ea635d45e9eb0fb6df6ec9f72ad71d975a97
Pavel <Pavel@DESKTOP-7B3ONOB>
2021-08-27 13:58:40 +0700

    WinAPI ListBox

--- WinAPI/WinAPI/Main.cpp
+++ WinAPI/WinAPI/Main.cpp
@@ -1,29 +1,63 @@
-#include <Windows.h>
-#include "resource.h"
+п»ї#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR* LIST_CONTENT[] =
+{
+	"This",
+	"is",
+	"my",
+	"first",
+	"ListBox"
+};
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, int nCmdShow)
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	//MessageBox(NULL, "Это мое первое окно, созданное в WinAPI", "Info MessageBox", MB_ICONINFORMATION | MB_YESNOCANCEL);
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1),0, (DLGPROC)DlgProc, 0);
+
+	//MessageBox(NULL, "Р­С‚Рѕ РјРѕРµ РїРµСЂРІРѕРµ РѕРєРЅРѕ, СЃРѕР·РґР°РЅРЅРѕРµ РІ WinAPI", "Info MessageBox", MB_ICONINFORMATION | MB_YESNOCANCEL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
+
 	return 0;
 }
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_INITDIALOG: 
+	case WM_INITDIALOG:
 	{
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		HICON hIcon = LoadIcon(GetModuleHandle(0), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 1, (LPARAM)hIcon);
+
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+
+		for (int i = 0; i < sizeof(LIST_CONTENT) / sizeof(LIST_CONTENT[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)LIST_CONTENT[i]);
+		}
 	}
-		break;
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK: MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDOK:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+
+			int i = SendMessageA(hList, LB_GETCURSEL, 0, 0);
+
+			//MessageBox(NULL, LIST_CONTENT[i], "Р’С‹ РІС‹Р±СЂР°Р»Рё", MB_ICONINFORMATION | MB_OK);
+
+			CHAR sz_buffer[256] = {};
+			SendMessageA(hList, LB_GETTEXT, 0, (LPARAM)sz_buffer);
+			MessageBox(NULL, sz_buffer, "Р’С‹ РІС‹Р±СЂР°Р»Рё", MB_ICONINFORMATION | MB_OK);
+
+			// IDOK: MessageBox(NULL, "Р‘С‹Р»Р° РЅР°Р¶Р°С‚Р° РєРЅРѕРїРєР° OK", "Info", MB_ICONINFORMATION | MB_OK);
+		}
+		break;
+
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 	}
